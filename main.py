dbb2998aea76f4c5cbfc8c3d8720eb6bcf3332dc
nagarajRPoojari <np137270@gmail.com>
2024-01-11 00:05:31 +0530

    streamlit

--- main.py
+++ main.py
@@ -1,43 +1,98 @@
 import streamlit as st
-st.set_page_config(layout="wide")
-st.title("this is a bmsit project")
-st.image("https://imgs.search.brave.com/Y2-lY7NA7o2rti-Uned1jQyOxQEs3XRFn9CT2z47chg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDY5NTA1/NDUuanBn")
-
 import pandas as pd
+import numpy as np
 import plotly.express as px
+import warnings
+from HybridQCCNN.config.configuration import ConfigurationManager
+from HybridQCCNN.pipeline import Pipeline , merge
+warnings.filterwarnings("ignore")
 
-# Read data from CSV file
-csv_file_path = "HybridResnet18.csv"
-df = pd.read_csv(csv_file_path)
+st.set_page_config(layout="wide")
+st.title("Hi , ")
 
-# Create a line chart with Plotly Express
-fig = px.line(df, x='Epochs', y=['Training Loss', 'Validation Loss', 'Training Accuracy', 'Validation Accuracy'],
-              labels={'value': 'Metrics'},
-              title='Training and Validation Metrics Over Epochs for resnet 18')
+@st.cache_resource()
+def load_model():
+    config=ConfigurationManager().get_model_trainer_config()
+    pipeline=Pipeline(config=config)
+    return pipeline
 
-# Customize the layout
-fig.update_layout(
-    xaxis_title='Epochs',
-    yaxis_title='Metrics',
-    legend_title='Metrics'
-)
 
-# Display the Plotly chart in Streamlit
-st.plotly_chart(fig)
-csv_file_path2 = "HybridResnet50.csv"
-df2= pd.read_csv(csv_file_path2)
+pipeline=load_model()
 
-# Create a line chart with Plotly Express
-fig2 = px.line(df2, x='Epochs', y=['Training Loss', 'Validation Loss', 'Training Accuracy', 'Validation Accuracy'],
-              labels={'value': 'Metrics'},
-              title='Training and Validation Metrics Over Epochs for resnet 50')
 
-# Customize the layout
-fig2.update_layout(
-    xaxis_title='Epochs',
-    yaxis_title='Metrics',
-    legend_title='Metrics'
-)
+c1,c2=st.columns(2)
 
-# Display the Plotly chart in Streamlit
-st.plotly_chart(fig2)
+selected_country = c2.selectbox("Select model", ["Resnet50", "Hybrid-quantum-Resnet50"])
+
+
+if selected_country == "Resnet50":
+    device = ["GPU"]
+elif selected_country == "Hybrid-quantum-Resnet50":
+    device = ["qiskit.aer", "default.qubit"]
+else:
+    device = []
+    
+
+selected_city = c2.selectbox("Select device", device)
+
+
+image=c1.file_uploader("Upload image",type=["jpg",'png','jpeg'])
+if image !=None and selected_country!=None:
+    res,prob=pipeline.inference(image)
+    c1.write(res)
+    c1.image(image, caption="Uploaded Image",width=200)
+
+    c2.bar_chart(prob,x='class')
+
+ops=["classical Resnet50 vs Hybrid Resnet50 ","classical Resnet18 vs Hybrid Resnet18 ","classical inception vs Hybrid inception "]
+graph=st.selectbox("select training graph",options=ops)
+
+col1,col2=st.columns(2)
+
+
+resnet50 = "results/resnet50.csv"
+resnet50 = pd.read_csv(resnet50)
+
+
+hybrid_resnet50 = "results/Hybridresnet50.csv"
+hybrid_resnet50 = pd.read_csv(hybrid_resnet50)
+
+
+resnet18 = "results/resnet18.csv"
+resnet18 = pd.read_csv(resnet18)
+
+
+hybrid_resnet18 = "results/HybridResnet18.csv"
+hybrid_resnet18 = pd.read_csv(hybrid_resnet18)
+
+
+inceptionv3 = "results/inceptionV3.csv"
+inceptionv3 = pd.read_csv(inceptionv3)
+
+
+hybrid_inceptionv3 = "results/HybridInceptionV3.csv"
+hybrid_inceptionv3 = pd.read_csv(hybrid_inceptionv3)
+
+
+
+    
+    
+
+col1.write("Training")
+col2.write("validation")
+if graph==ops[0]:
+    a,b=merge(resnet50,hybrid_resnet50,'resnet50')
+    
+    col1.line_chart(a)
+    col2.line_chart(b)
+elif graph==ops[1]:
+    a,b=merge(resnet18,hybrid_resnet18,'resnet18')
+    
+    col1.line_chart(a)
+    col2.line_chart(b)
+
+elif graph==ops[2]:
+    a,b=merge(inceptionv3,hybrid_inceptionv3,'InceptionV3')
+    
+    col1.line_chart(a)
+    col2.line_chart(b)
--- src/HybridQCCNN/pipeline/__init__.py
+++ src/HybridQCCNN/pipeline/__init__.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import os
+import pandas as pd 
 
 # pipeline only for inferencing
 class Pipeline:
@@ -24,5 +25,33 @@ class Pipeline:
         img=image[np.newaxis , :]
         res=self.loaded_model(img)
         id=np.argmax(res)
-        return self.classes[id]
+        
+
+        df=pd.DataFrame({
+            'probability':res[0],
+            'class':self.classes
+        })
+        
+        
+        return self.classes[id] , df
             
+
+def merge(a,b,s):
+    f='classical '
+    s='quantum '
+    train1=a[['Training Loss','Training Accuracy']]
+    train1.rename(columns={'Training Loss':f+' Loss' ,'Training Accuracy':f+' Accuracy'},inplace=True)
+    train2=b[['Training Loss','Training Accuracy']]
+    train2.rename(columns={'Training Loss':s+' Loss' ,'Training Accuracy':s+' Accuracy'},inplace=True)
+    
+    Loss=pd.concat([train1,train2],axis=1)
+    
+    val1=a[['Validation Loss','Validation Accuracy']]
+    val1.rename(columns={'Validation Loss':f+' Loss' ,'Validation Accuracy':f+' Accuracy'},inplace=True)
+    val2=b[['Validation Loss','Validation Accuracy']]
+    val2.rename(columns={'Validation Loss':s+' Loss' ,'Validation Accuracy':s+' Accuracy'},inplace=True)
+    
+    Accuracy=pd.concat([val1,val2],axis=1)
+    
+
+    return Loss,Accuracy
